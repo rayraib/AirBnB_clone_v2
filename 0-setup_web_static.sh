73eff850b2873a63a6170358ef8a3c027d756799
rayraib <binitarai11@gmail.com>
2018-03-31 07:22:16 +0000

    change code variable and location block

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
-code="\\n    location /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t}\n"
+code="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
@@ -8,7 +8,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 echo "test index" > /data/web_static/releases/test/index.html
 sudo touch /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/index.html /data/web_static/current 
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "25i $code" /etc/nginx/sites-enabled/default
 sudo service nginx restart
