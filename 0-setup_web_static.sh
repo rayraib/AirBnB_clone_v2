221bab34d9ebba2a04c92a402869cf4d9dc8ce7c
rayraib <binitarai11@gmail.com>
2018-03-31 07:14:38 +0000

    update code variable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
-code="\n    location /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t}\n"
+code="\\n    location /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t}\n"
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
@@ -10,5 +10,5 @@ echo "test index" > /data/web_static/releases/test/index.html
 sudo touch /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/index.html /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "25i $code" /etc/nginx/sites-available/default
+sudo sed -i "25i $code" /etc/nginx/sites-enabled/default
 sudo service nginx restart
