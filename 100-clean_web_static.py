16b26e79b8a56c8573cbe864fa8ce5d902168910
rayraib <binitarai11@gmail.com>
2018-03-31 13:23:52 +0000

    return False if failure

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,6 +8,7 @@ from fabric.api import run, env, hosts
 env.hosts = ['52.23.184.231', '54.173.122.229']
 env.user = 'ubuntu'
 
+
 def do_clean(number=0):
     '''
         delete old archive files
@@ -15,5 +16,11 @@ def do_clean(number=0):
     if number == 0:
         number = number + 2
     number = number + 1
-    run(ls -t /versions | tail -n +number| xargs rm)
-    run(ls -t /data/web_static/releases | tail -n +number| xargs rm)
+    result = run(ls -t /versions | tail -n +number| xargs rm)
+    if result.failed:
+        return False
+    result = run(ls -t /data/web_static/releases | tail -n +number| xargs rm)
+    if result.failed:
+        return False
+
+    return True
