7d681b6921fdf7c117a59e26a3bee83a09180c31
rayraib <binitarai11@gmail.com>
2018-03-31 06:38:13 +0000

    change code variable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
-code="
-    location /hbnb_static/ {
-        alias /data/web_static/current;
-    }
-    "
+code="\n    location /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t
+    }\n"
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
