0796e99ed49c58444baedb00edbf868583a0e7ca
rayraib <binitarai11@gmail.com>
2018-03-31 06:18:02 +0000

    sudo everything

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,11 @@
 code="\\tlocation /hbnb_static/ {\n\talias /data/web_static/current;\n\t}"
 sudo apt-get update -y
 sudo apt-get install nginx -y
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
 echo "test index" > /data/web_static/releases/test/index.html
-touch /data/web_static/current
-ln -sf /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
-sed -i "28i $code" /etc/nginx/sites-available/default
+sudo touch /data/web_static/current
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "28i $code" /etc/nginx/sites-available/default
