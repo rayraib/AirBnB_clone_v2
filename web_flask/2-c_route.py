acf8ee68fba0b245ef133990dc8b5720a08e8e8a
rayraib <binitarai11@gmail.com>
2018-04-10 03:23:48 +0000

    pep8 compliant

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 '''
-	Starts a Flask web application
+    Starts a Flask web application
 '''
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return('Hello HBNB!')
+    return('Hello HBNB!')
+
 
 @app.route('/hbnb')
 def hbnb():
-	return('HBNB!')
+    return('HBNB!')
+
 
 @app.route('/c/<text>')
 def c_text(text):
-	return ('C %s') %text.replace('_', ' ')
+    return ('C %s') %text.replace('_', ' ')
+
 
 if __name__ == '__main__':
-	app.strict_slashes=False
-	app.run(host = '0.0.0.0', port=5000)
+    app.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
