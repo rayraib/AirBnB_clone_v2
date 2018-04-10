b93e5ea09b5dcac7328b5e5a488e7a85eca5f264
rayraib <binitarai11@gmail.com>
2018-04-10 03:18:02 +0000

    pep8 compliant

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 '''
-	Starts a Flask web application
+    Starts a Flask web application
 '''
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return('Hello HBNB!')
+    '''
+        Return the index page
+    '''
+    return ('Hello HBNB!')
+
 
 if __name__ == '__main__':
-	app.strict_slashes=False
-	app.run(host = '0.0.0.0', port=5000)
+    app.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
