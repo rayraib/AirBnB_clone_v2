dbbab7fe4924647a60b15b72217aa8e90bcdac22
rayraib <binitarai11@gmail.com>
2018-04-10 03:21:13 +0000

    pep8 compliant

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 '''
-	Starts a Flask web application
+    Starts a Flask web application
 '''
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return('Hello HBNB!')
+    return ('Hello HBNB!')
+
 
 @app.route('/hbnb')
 def hbnb():
-	return('HBNB!')
+    return ('HBNB!')
+
 
 if __name__ == '__main__':
-	app.strict_slashes=False
-	app.run(host = '0.0.0.0', port=5000)
+    app.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
