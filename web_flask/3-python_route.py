61cd38fc5c7e008d4a597eea8b220c8287eadcf4
rayraib <binitarai11@gmail.com>
2018-04-17 19:45:38 +0000

    python roue

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 '''
-	Starts a Flask web application
+    Starts a Flask web application
 '''
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return('Hello HBNB!')
+    return('Hello HBNB!')
+
 
 @app.route('/hbnb')
 def hbnb():
-	return('HBNB!')
+    return('HBNB!')
+
 
 @app.route('/c/<text>')
 def c_text(text):
-	return ('C %s') %text.replace('_', ' ')
+    return ('C %s') % text.replace('_', ' ')
+
 
 @app.route('/python/<text>')
 def python_text(text='is cool'):
-	return ('python %s') %text.replace('_', ' ')
+    return ('python %s') % text.replace('_', ' ')
+
 
 if __name__ == '__main__':
-	app.strict_slashes=False
-	app.debug = 1
-	app.run(host = '0.0.0.0', port=5000)
+    app.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
