1d4871f53f1dfdf3b9b1bb758f77b4c25c6e7130
rayraib <binitarai11@gmail.com>
2018-04-18 00:57:22 +0000

    format presentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,19 @@ from flask import render_template
 app = Flask(__name__)
 
 storage.reload()
-data = storage.all()
-print(data)
+all_states = storage.all('State')
+states = all_states.values()
+
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
-   return render_template('7-states_list.html', data=data)
+   return render_template('7-states_list.html',states=states)
 
+'''
 @app.teardown_appcontext
 def tear_down(storage):
     storage.close()
-
+'''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
